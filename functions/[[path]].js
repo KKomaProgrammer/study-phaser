a234871099ff5267621e0c2c289f8405740c5037
KKoma Programmer <ijunu3343@gmail.com>
2026-06-21 14:57:07 +0900

    Preserve original SPA route on direct entry

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -11,5 +11,16 @@ export async function onRequest({ request, env }) {
   }
 
   const indexUrl = new URL('/index.html', url.origin);
-  return env.ASSETS.fetch(new Request(indexUrl.toString(), request));
+  const indexResponse = await env.ASSETS.fetch(new Request(indexUrl.toString(), request));
+  let html = await indexResponse.text();
+  const originalPath = JSON.stringify(url.pathname + url.search + url.hash);
+  html = html.replace('<head>', `<head><script>window.__SPA_INITIAL_PATH__=${originalPath};history.replaceState(null,'',window.__SPA_INITIAL_PATH__);</script>`);
+
+  return new Response(html, {
+    status: 200,
+    headers: {
+      'content-type': 'text/html; charset=utf-8',
+      'cache-control': 'no-store'
+    }
+  });
 }
