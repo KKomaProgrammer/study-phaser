dafc378a9146ba53c95b0cb0d709f06a66b76890
KKoma Programmer <ijunu3343@gmail.com>
2026-06-22 15:12:49 +0900

    Normalize course fallback loading screen

--- functions/course/[[id]].js
+++ functions/course/[[id]].js
@@ -10,7 +10,7 @@ function fallbackHtml() {
   <script src="https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/min/vs/loader.js"></script>
 </head>
 <body>
-  <div id="app"><main class="loading-screen"><div class="brand-orb">P</div><p>Phaser 문법을 불러오는 중...</p></main></div>
+  <div id="app" class="app-root"><main class="boot-screen"><div class="brand-orb">P</div><p>Phaser 문법 검색기를 불러오는 중입니다</p></main></div>
   <script type="module" src="/js/app.js"></script>
   <script type="module" src="/js/readable-code.js"></script>
   <script type="module" src="/js/suggestion-nav-fix.js"></script>
@@ -19,7 +19,7 @@ function fallbackHtml() {
 }
 
 export async function onRequest({ request, env }) {
-  const url = new URL('/404.html', new URL(request.url).origin);
+  const url = new URL('/index.html', new URL(request.url).origin);
   let html = '';
 
   try {
