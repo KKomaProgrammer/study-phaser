4736fbefd5455de825b3f37a8a5cd1cef7e176bf
KKoma Programmer <ijunu3343@gmail.com>
2026-06-27 13:13:05 +0900

    Version course fallback assets

--- functions/course/[[id]].js
+++ functions/course/[[id]].js
@@ -5,15 +5,16 @@ function fallbackHtml() {
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>Phaser 문법 검색</title>
-  <link rel="stylesheet" href="/css/styles.css" />
-  <link rel="stylesheet" href="/css/overrides.css" />
+  <link rel="stylesheet" href="/css/styles.css?v=20260627_3" />
+  <link rel="stylesheet" href="/css/overrides.css?v=20260627_3" />
+  <link rel="stylesheet" href="/css/extra.css?v=20260627_3" />
   <script src="https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/min/vs/loader.js"></script>
 </head>
 <body>
   <div id="app" class="app-root"><main class="boot-screen"><div class="brand-orb">P</div><p>Phaser 문법 검색기를 불러오는 중입니다</p></main></div>
-  <script type="module" src="/js/app.js"></script>
-  <script type="module" src="/js/readable-code.js"></script>
-  <script type="module" src="/js/suggestion-nav-fix.js"></script>
+  <script type="module" src="/js/app.js?v=20260627_3"></script>
+  <script type="module" src="/js/readable-code.js?v=20260627_3"></script>
+  <script type="module" src="/js/suggestion-nav-fix.js?v=20260627_3"></script>
 </body>
 </html>`;
 }
